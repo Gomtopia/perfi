1a3b67ced5efa6a664ce34fa894426a3ae39ad90
Sunyoung Jeong <jsy4754@gmail.com>
2018-06-24 01:36:58 -0700

    Add Account, Entry model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class Account(models.Model):
+    ASSETS = 'ASSETS'
+    LIABILITIES = 'LIABILITIES'
+    INCOME = 'INCOME'
+    EXPENSES = 'EXPENSES'
+    ACCOUNT_TYPE_CHOICES = (
+        (ASSETS, 'Assets'),
+        (LIABILITIES,'Liabilities'),
+        (INCOME, 'Income'),
+        (EXPENSES, 'Expenses'),
+    )
+
+    name = models.CharField(max_length=128, primary_key=True)
+    account_type = models.CharField(
+        max_length = 16,
+        choices = ACCOUNT_TYPE_CHOICES,
+        default = ASSETS,
+    )
+
+class Entry(models.Model):
+    debit = models.ForeignKey(Account, related_name='debit_account', on_delete=models.PROTECT)
+    credit = models.ForeignKey(Account, related_name='credit_account', on_delete=models.PROTECT)
+    description = models.CharField(max_length=256)
+    amount = models.DecimalField(max_digits=19, decimal_places=10)
+    date = models.DateTimeField()
